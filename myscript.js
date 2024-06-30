d42ff5451ea47d8c0232f3a15dce4835b0a0d68f
Ayon-exe <146752822+Ayon-exe@users.noreply.github.com>
2024-06-30 19:54:27 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,41 +3,6 @@ function signin(){
     	document.getElementById("demo1").innerHTML="Your username is: "+document.getElementById("password").value;
 	window.location.href = "./homepage.html";
 	}
-function generate(){
-    	document.getElementById("currprice").value= (Math.random() * 100).toFixed(2);
-	xvalues.push(document.getElementById("currprice").value);
-	}
-    
-function buy(){
-    	document.getElementById("wallet").value=(document.getElementById("wallet").value)-(document.getElementById("currprice").value);
-    }
-    
-function sell(){
-    	(document.getElementById("wallet").value)= parseFloat(document.getElementById("currprice").value)+ parseFloat(document.getElementById("wallet").value);
-    }
-
-const xValues = [];
-const yValues = [10,20,30,40,50,60,70,80,,90,100];
-
-new Chart("myChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      fill: false,
-      lineTension: 0,
-      backgroundColor: "rgba(0,0,255,1.0)",
-      borderColor: "rgba(0,0,255,0.1)",
-      data: yValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    scales: {
-      yAxes: [{ticks: {min: 6, max:16}}],
-    }
-  }
-});
 
 const container = document.getElementById('container');
 const signupBtn = document.getElementById('signup');
